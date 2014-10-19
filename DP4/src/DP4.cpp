a570710bfaf03c9d54312ae2f313c1f7a98af4ac
aldgoff <aldgoff@ALDGOFF-MOBL1.amr.corp.intel.com>
2014-10-18 22:39:44 -0700

    Commit from PC Eclipse DP4 C++ project.

---
+++ DP4/src/DP4.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : DP4.cpp
+// Author      : Allan Goff
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
